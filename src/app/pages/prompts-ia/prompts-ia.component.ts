24cee5d5bfb942450b83e29daf316a2fa4fd94b2
Steven Galo <stevengalocr@gmail.com>
2025-07-19 21:11:52 -0600

    What's included? Screen ready

--- src/app/pages/prompts-ia/prompts-ia.component.ts
+++ src/app/pages/prompts-ia/prompts-ia.component.ts
@@ -1,19 +1,17 @@
-import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { Component, Inject, PLATFORM_ID, ViewChild, ElementRef, OnInit, OnDestroy } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { packItems } from '../../data/packItems';
 
-import { OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-prompts-ia',
   standalone: true,
   imports: [CommonModule, HeaderComponent],
   templateUrl: './prompts-ia.component.html',
-  styleUrls: ['./prompts-ia.component1.scss', './prompts-ia.component2.scss'],
+  styleUrls: ['./prompts-ia.component1.scss', './prompts-ia.component2.scss', './prompts-ia.component3.scss'],
 })
-
 export class PromptsIAComponent {
   packItems = packItems;
 }
